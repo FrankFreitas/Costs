586f7a4e0b93645a87d95def4f0b2612131fec45
Frank Freitas <frankfreitas664@gmail.com>
2024-01-29 23:41:45 -0300

    resgatando projetos do banco de dados e estilizando o dashboard dos projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,15 +3,31 @@ import styles from './Projects.module.css'
 import Message from '../layout/Message'
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
+import ProjectCard from '../projects/ProjectCard'
 import { useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 function Projects() {
+    const [projects, setProjects] = useState([])
+
     const location = useLocation()
-    console.log(location.state)
     let message = ""
     if (location.state) {
         message = location.state.message
     }
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application.json',
+            },
+        }).then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                setProjects(data)
+            }).catch(err => console.log(err))
+    }, [])
+
 
     return (
         <div className={styles.project_container}>
@@ -21,7 +37,16 @@ function Projects() {
             </div>
             {message && <Message type='success' msg={message} />}
             <Container customClass='start'>
-                <p>Projetos...</p>
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard
+                        id = {project.id}
+                        name ={project.name}
+                        budget = {project.budget}
+                        category={project.category.name}
+                        key={project.id}
+                        />
+                    ))}
             </Container>
         </div>
     )
