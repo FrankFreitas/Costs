84e21d0fc0fc867094344e1d3c1eae6b98608320
Frank Freitas <frankfreitas664@gmail.com>
2024-01-31 19:41:52 -0300

    criando área de projetos

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,6 +1,6 @@
 import styles from './Project.module.css'
 
-import {  useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
@@ -13,6 +13,7 @@ function Project() {
     const { id } = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -33,6 +34,8 @@ function Project() {
     }, [id])
 
     function editPost(project) {
+        setMessage('')
+
         //budget validation
         if (project.budget < project.cost) {
             //message
@@ -62,12 +65,17 @@ function Project() {
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm)
     }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm)
+    }
+
     return (
         <>
             {project.name ? (
                 <div className={styles.project_details}>
                     <Container customClass='column'>
-                        {message && <Message type= {type} msg={message}/>}
+                        {message && <Message type={type} msg={message} />}
                         <div className={styles.details_container}>
                             <h1>Projeto:{project.name}</h1>
                             <button className={styles.btn} onClick={toggleProjectForm}>
@@ -94,6 +102,24 @@ function Project() {
                                 </div>
                             )}
                         </div>
+                        <div className={styles.service_form_container}>
+                            <h2>Adicione um serviço:</h2>
+                            <button className={styles.btn} onClick={toggleServiceForm}>
+                                {!showServiceForm ? "Adicionar serviço" : "Fechar"}
+                            </button>
+                            <div className={styles.project_info}>
+                                {showServiceForm && (
+                                    <div>
+                                        formulário do servico
+                                    </div>
+                                )
+                                }
+                            </div>
+                        </div>
+                        <h2>Serviços</h2>
+                        <Container customClass='start'>
+                            <p>Itens de Serviço</p>
+                        </Container>
                     </Container>
                 </div>
 
